be87bf3b639d7c39e559ad7400b8991c03c26fe3
Kotikalapudi <raghakot@amazon.com>
2018-05-31 18:09:08 -0700

    better impl of problem 5

--- 0-50/5.py
+++ 0-50/5.py
@@ -1,33 +1,51 @@
 from collections import defaultdict, Counter
 
 
+def factorize(n, cache):
+    if n in cache:
+        return cache[n]
+
+    factors = Counter()
+    f = 2
+    while f * f <= n:
+        if n in cache:
+            factors = factors + cache[n]
+            break
+
+        count = 0
+        while n % f == 0:
+            n /= f
+            count += 1
+
+        if count > 0:
+            factors[f] = count
+        f += 1
+
+    if n > 1:
+        factors[n] = 1
+
+    cache[n] = factors
+    return factors
+
+
 def evenly_divisible(n):
     """ Idea:
     - Find factors of numbers 1 to n. Use DP to cache results bottom up.
     - Amongst all factors, we have to include max counts of prime factors.
         - For example, in in 1 .. 10, 2 has to be included 3 times since 8 = 2 ^ 3
     """
-    factors = defaultdict(list)
-    prime_counter = defaultdict(int)
+    max_counts = Counter()
+    cache = dict()
 
-    # Recurrence to get factors
-    # factors(n) = factors(n/i) + [i] if n % i == 0; i in [2, n]
-    for n in range(2, n+1):
-        for i in range(n-1, 1, -1):
-            if n % i == 0:
-                factors[n] = factors[n/i] + [i]
+    for n in range(n, 1, -1):
+        factors = factorize(n, cache)
 
-        # If the above didnt work, n must have been prime.
-        if len(factors[n]) == 0:
-            factors[n] = [n]
-
-        # Update counters
-        counts = Counter(factors[n])
-        for k, v in counts.iteritems():
-            prime_counter[k] = max(prime_counter[k], v)
+        # Update max counts
+        for k, v in factors.iteritems():
+            max_counts[k] = max(max_counts[k], v)
 
     res = 1
-    for k, v in prime_counter.iteritems():
+    for k, v in max_counts.iteritems():
         res *= k ** v
     return res
 
